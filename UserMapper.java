f7c0090c90df581faa65752594f7d1a64ab9434f
sujanpok <97655524+sujanpok@users.noreply.github.com>
2022-02-03 16:23:18 +0900

    Create UserMapper.java

---
+++ UserMapper.java
@@ -0,0 +1,19 @@
+package com.example.demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.dataInput.Input;
+import com.example.demo.dataInput.UserInfo;
+
+
+
+
+@Mapper
+public interface  UserMapper {
+	
+
+	int doCheck(Input request);
+
+	
+	UserInfo getUserInfo(Input request);
+}
