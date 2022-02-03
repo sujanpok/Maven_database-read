b690dfa9c4cfb144ecc8db3a238645f7804abc4d
sujanpok <97655524+sujanpok@users.noreply.github.com>
2022-02-03 16:22:00 +0900

    Create main.java

---
+++ main.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dataInput.Input;
+import com.example.demo.service.LoginService;
+
+import lombok.extern.slf4j.Slf4j;
+
+	@Controller
+	@Slf4j
+	public class Main {
+
+	    @Autowired
+	    private LoginService service;
+
+		
+	    @GetMapping("/user")
+	    public String hello(Model model) {
+	        return "user";
+	    }
+	    
+	    @RequestMapping("/regi")
+	    public String defectDetails() {
+	        return "regi";
+	    }
+	    
+	    
+	    
+	    
+	    @PostMapping("/logined")
+	    public String loginCheck(HttpSession session,Input request,Model model) {
+	    	
+	    	service.checklogin(request,model);
+	    
+	    	
+	    	return "hello2";
+	    	
+	    }
+	    
+	    
+	    
+	    }
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
